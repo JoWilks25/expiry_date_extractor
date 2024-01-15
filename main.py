93688455fd3f16bba0b227f7d48bd84a77c405a7
JoWilks25 <58911477+JoWilks25@users.noreply.github.com>
2024-01-15 10:44:40 +0000

    Generate text file with output expiryDate,itemName,units & fix substitutions (#2)

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturda
 
 def generate_item_date_dict(section_text):
   # Remove the "Section Title" from the list
+  print("section_text", section_text)
   split_section_text = section_text.strip().split("\n")[1:]
+  print("split_section_text", split_section_text)
   expiry_items = {}
   current_day = None
   merge_with_next_line = False
@@ -56,13 +58,16 @@ def main():
   # 2. Cupboard
   # 3. Freezer
   # 4. Age-restricted products (assume no expiry date)
-  # 5. Offer savings (Assume should always exist)
+  # 5. Substituted items
+  # 6. Offer savings (Assume should always exist)
   fridge_index_start= all_text.find("Fridge")
   cupboard_index_start = all_text.find("Cupboard")
   freezer_index_start = all_text.find("Freezer")
   age_restrict_index_start = all_text.find("Age-restricted products")
-  offer_savings_index_start = all_text.find("Offers savings")
+  substituted_items_start = all_text.find("Substituted items")
+  offer_savings_index_start = all_text.find("Offers savings", fridge_index_start) # Start at Fridge text so don't get offer savings from grey box
 
+  # TODO - Replace the bazillion if else statements
   # If not found index = -1
   fridge_index_end = None
   if fridge_index_start == -1:
@@ -73,6 +78,8 @@ def main():
     fridge_index_end = freezer_index_start
   elif age_restrict_index_start != -1:
     fridge_index_end = age_restrict_index_start
+  elif substituted_items_start != -1:
+    fridge_index_end = substituted_items_start
   elif offer_savings_index_start != -1:
     fridge_index_end = offer_savings_index_start
 
@@ -83,27 +90,47 @@ def main():
     cupboard_index_end = freezer_index_start
   elif age_restrict_index_start != -1:
     cupboard_index_end = age_restrict_index_start
+  elif substituted_items_start != -1:
+    fridge_index_end = substituted_items_start
   elif offer_savings_index_start != -1:
     cupboard_index_end = offer_savings_index_start
-
 
   freezer_index_end = None
   if freezer_index_start == -1:
     print("Freezer section not found")
   elif age_restrict_index_start != -1:
     freezer_index_end = age_restrict_index_start
+  elif substituted_items_start != -1:
+    freezer_index_end = substituted_items_start
   elif offer_savings_index_start != -1:
     freezer_index_end = offer_savings_index_start
+
+  age_restrict_index_end = None
+  if age_restrict_index_start == -1:
+    print("Age Restricted section not found")
+  elif substituted_items_start != -1:
+    age_restrict_index_end = substituted_items_start
+  elif offer_savings_index_start != -1:
+    age_restrict_index_end = offer_savings_index_start
+
+  substituted_items_end = None
+  if substituted_items_start == -1:
+    print("Substituted items section not found")
+  elif offer_savings_index_start != -1:
+    substituted_items_end = offer_savings_index_start
 
   # Extract text from each section
   fridge_text = all_text[fridge_index_start:fridge_index_end]
   cupboard_text = all_text[cupboard_index_start:cupboard_index_end]
   freezer_text = all_text[freezer_index_start:freezer_index_end]
+  substituted_text = all_text[substituted_items_start:substituted_items_end]
 
   # Get each specific item under each section
   fridge_dict = generate_item_date_dict(fridge_text)
   cupboard_dict = generate_item_date_dict(cupboard_text)
   freezer_dict = generate_item_date_dict(freezer_text)
+  substituted_dict = generate_item_date_dict(substituted_text)
+  print("substituted_dict", substituted_dict)
 
   # Merge all Dicts
   all_items_dict = {}
@@ -124,8 +151,14 @@ def main():
       all_items_dict[key].extend(freezer_dict[key])
     else:
       all_items_dict[key] = freezer_dict[key]
+  
+  for key in substituted_dict.keys():
+    if key in all_items_dict:
+      all_items_dict[key].extend(substituted_dict[key])
+    else:
+      all_items_dict[key] = substituted_dict[key]
 
-  print(all_items_dict)
+  # print(all_items_dict)
 
   # Determine delivery date
   delivery_date_start = all_text.find("Delivery date:") + 14
@@ -140,8 +173,9 @@ def main():
     date = None
     if key == "Over one week":
       date = (delivery_date + timedelta(days=7)).strftime("%d/%m/%Y")
-    elif key == "No use-by date":
-      date = (delivery_date + timedelta(days=30)).strftime("%d/%m/%Y")
+    # Exclude items with no use-by date
+    # elif key == "No use-by date":
+    #   date = (delivery_date + timedelta(days=30)).strftime("%d/%m/%Y")
     elif key in days_of_week:
         date = (next_weekday(delivery_date, days_of_week.index(key))).strftime("%d/%m/%Y")
     
@@ -150,16 +184,29 @@ def main():
   
   print(final_items_date_dict)
 
-  # Convert to CSV format
-  headers = ["Subject", "Start Date", "Description"]
+  # Convert to Text format ExpiryDate,itemName,units
   event_rows = []
   for dateString, itemList in final_items_date_dict.items():
-    items_string = (' - ').join(itemList)
-    event_rows.append(["Expiring Today", dateString, f" - {items_string}"])
+    for item in itemList:
+      split_name = item.split('/')
+      itemName = ''
+      units = ''
+      # for item with multiple units i.e. 
+      # 'ARLA LACTOFREE SEMI SKIMMED MILK DRINK 2l (£3.00/EACH)2/26.00' = ['ARLA LACTOFREE SEMI SKIMMED MILK DRINK 2l (£3.00', 'EACH)2', '26.00']
+      if len(split_name) > 2:
+        itemName = split_name[0]
+        units = split_name[len(split_name) - 2][-1] # Always get the number before the last /
+      else:
+        # "JACKSON'S SUPER SEEDED BROWN BLOOMER 800g1/12.00" = ["JACKSON'S SUPER SEEDED BROWN BLOOMER 800g1", '12.00']
+        itemName = split_name[0][0:-1]
+        units = split_name[0][-1]
+      # if number of units is 0 (i.e. because of a substitution) don't add
+      if int(units) > 0:
+        event_rows.append([dateString, f"{itemName}", f"{units}"])
+        
 
   # Write to CSV file
   with open("expiry_dates.txt", "w") as f:
-    f.write(",".join(headers) + "\n")
     for row in event_rows:
       f.write(",".join(row) + "\n")
   
